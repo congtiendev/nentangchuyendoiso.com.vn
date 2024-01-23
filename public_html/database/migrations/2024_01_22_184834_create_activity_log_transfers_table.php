731668ade909ba13d0b01a21ff2655c7bc829bea
TranChiCong157 <congtc157@gmail.com>
2024-01-23 14:39:20 +0700

    update lich su mien nhiem + thuyen chuyen

---
+++ public_html/database/migrations/2024_01_22_184834_create_activity_log_transfers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activity_log_transfers', function (Blueprint $table) {
+            $table->id();
+            $table->string('action_type');
+            $table->unsignedBigInteger('user_id');
+            $table->string('user_type');
+            $table->unsignedBigInteger('employee_id');
+            $table->string('log_type');
+            $table->text('remark');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activity_log_transfers');
+    }
+};
