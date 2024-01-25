c9597317b0eecde08dd092fa00c131a76358840b
TranChiCong157 <congtc157@gmail.com>
2024-01-25 17:15:42 +0700

    update log ky luat

--- public_html/Modules/Hrm/Http/Controllers/CompanyPolicyController.php
+++ public_html/Modules/Hrm/Http/Controllers/CompanyPolicyController.php
@@ -6,11 +6,12 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
-use Modules\Hrm\Entities\Branch;
+use Modules\Hrm\Entities\Branch;  
 use Modules\Hrm\Entities\CompanyPolicy;
 use Modules\Hrm\Events\CreateCompanyPolicy;
 use Modules\Hrm\Events\DestroyCompanyPolicy;
 use Modules\Hrm\Events\UpdateCompanyPolicy;
+use App\Models\ActivityLogCompanyPolicy;
 
 class CompanyPolicyController extends Controller
 {
@@ -26,6 +27,16 @@ class CompanyPolicyController extends Controller
         } else {
             return redirect()->back()->with('error', __('Permission denied.'));
         }
+    }
+
+    public function activityLogPolicy()
+    {
+        if (Auth::user()->isAbleTo('companypolicy manage')) {
+
+            $policys = ActivityLogCompanyPolicy::latest()->get();
+            return view('hrm::companyPolicy.activityLog', compact('policys'));
+        }
+
     }
 
     /**
@@ -84,6 +95,20 @@ class CompanyPolicyController extends Controller
             $policy->created_by  = creatorId();
             $policy->save();
 
+            $action = 'Tạo kỷ luật';
+            $typeAction = 'store';
+            $branchName = Branch::find($policy->branch)->name;
+            $changes = [
+                'user_id'    => $request->employee_id,
+                'title'      =>  $policy->title,
+                'branchName' => $branchName,
+                'workspace'  => getActiveWorkSpace(),
+                'changed_by' => Auth::user()->name,
+                'changed_at' => now()->format('H:i:s d-m-Y'),
+            ];
+            $this->saveLog($policy, $changes, $action,$typeAction);
+
+
             event(new CreateCompanyPolicy($request, $policy));
 
 
@@ -140,6 +165,7 @@ class CompanyPolicyController extends Controller
                 $messages = $validator->getMessageBag();
                 return redirect()->back()->with('error', $messages->first());
             }
+            $originalData = $companyPolicy->getOriginal();
 
             if (isset($request->attachment)) {
                 if (!empty($companyPolicy->attachment)) {
@@ -165,6 +191,13 @@ class CompanyPolicyController extends Controller
                 $companyPolicy->attachment = $url;
             }
             $companyPolicy->save();
+
+            $action = 'Cập nhật kỷ luật';
+            $typeAction = 'update';
+            $newData = $companyPolicy->fresh()->getAttributes();
+            $changes = $this->getChanges($originalData, $newData);
+            $this->saveLog($companyPolicy, $changes, $action,$typeAction);
+
             event(new UpdateCompanyPolicy($request, $companyPolicy));
             return redirect()->route('company-policy.index')->with('success', __('Company policy successfully updated.'));
         } else {
@@ -185,6 +218,25 @@ class CompanyPolicyController extends Controller
                     delete_file($companyPolicy->attachment);
                 }
                 event(new DestroyCompanyPolicy($companyPolicy));
+                $action = 'Xóa kỷ luật';
+                $typeAction = 'delete';
+
+                $title = $companyPolicy->title;
+                $branch = Branch::find($companyPolicy->branch)->name;
+                $changes = [
+                    'title'      => $title,
+                    'branch'      => $branch,
+                    'changed_by' => Auth::user()->name,
+                    'changed_at' => now()->format('H:i:s d-m-Y'),
+                ];
+                 $logData = [
+                                'action_type' => $typeAction,
+                                'type' => get_class($companyPolicy),
+                                'log_type' => $action,
+                                'remark' => json_encode(['changes' => $changes]),
+                            ];
+                            ActivityLogCompanyPolicy::create($logData);
+
                 $companyPolicy->delete();
                 return redirect()->route('company-policy.index')->with('success', __('Company policy successfully deleted.'));
             } else {
@@ -193,5 +245,66 @@ class CompanyPolicyController extends Controller
         } else {
             return redirect()->back()->with('error', __('Permission denied.'));
         }
+    }
+
+    private function getChanges($originalData = null, $newData = null)
+    {
+        $changes = [];
+    
+        foreach ($originalData as $key => $value) {
+            if ($key === 'updated_at' || $key === 'created_at') {
+                continue;
+            }
+    
+            $oldValue = $originalData[$key];
+            $newValue = $newData[$key];
+    
+            // Check if it's a select field
+            if ($this->isSelectField($key)) {
+                
+                $oldName = $this->getNameFromDatabase($key, $oldValue);
+                $newName = $this->getNameFromDatabase($key, $newValue);
+
+            } else {
+                $oldName = $oldValue;
+                $newName = $newValue;
+            }
+    
+            if ($oldName !== $newName) {
+                $changes[$key] = [
+                    'old' => $oldName,
+                    'new' => $newName,
+                    'changed_by' => Auth::user()->name,
+                    'changed_at' => now()->format('H:i:s d-m-Y'),
+                ];
+            }
+        }
+    
+        return $changes;
+    }
+    private function isSelectField($key)
+    {
+        $selectFields = ['branch'];
+        return in_array($key, $selectFields);
+    }
+
+    private function getNameFromDatabase($key, $id)
+    {
+        switch ($key) {
+            case 'branch':
+                return Branch::find($id)->name;
+            default:
+                return $id;
+        }
+    }
+    private function saveLog($policy, $changes, $action, $typeAction)
+    {
+        $logData = [
+            'action_type' => $typeAction,
+            'type' => get_class($policy),
+            'log_type' => $action,
+            'remark' => json_encode(['changes' => $changes]),
+        ];
+        ActivityLogCompanyPolicy::create($logData);
     }
 }
